74acd90058e47e278be536be54106f20058ad50d
muratmalaev <malaev.m1769@gmail.com>
2023-11-18 16:13:24 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <navbar></navbar>
+  <home></home>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Home from './views/Home.vue'
+
+export default {
+  components: {
+    Navbar,
+    Home
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div>
+            <li v-for="phone in phones">
+                {{phone.Name}}
+            </li>
+        </div>
+        <button 
+            class="btn btn-primary"
+            @click="click"
+
+        >Click</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            phones: []
+        }
+    },
+    methods: {
+        click() {
+            console.log(axios)
+            const apiUrl = 'https://localhost:7046/Phone/get';
+
+            axios.get(apiUrl)
+            .then(response => {
+                console.log(response);
+                this.phones = response.data;
+            })
+            .catch(error => {
+                console.error('Ошибка при получении данных:', error);
+            });
+        }
+    }
+}
+</script>
